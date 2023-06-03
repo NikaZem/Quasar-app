0ec077475968e02b6a00fdc762e5c2de62faa579
Veronika <nikazemlakova38@gmail.com>
2023-06-03 20:29:33 +0300

    do navbar

--- quasar-todolist/src/layouts/MainLayout.vue
+++ quasar-todolist/src/layouts/MainLayout.vue
@@ -11,36 +11,72 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">My Todo List</div>
+        <div class="text-subtitle">{{ todaysDate }}</div>
+      </div>
+      <q-img class="header-image absolute-top" src="../assets/purple-yeti-cartoon-sitting-on-chair.jpg" />
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 185px); margin-top: 185px; border-right: 1px solid #ddd">
+          <q-list padding>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+            <q-item to="/" exact clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+
+             <q-item to="/help" exact clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+
+             <q-item to="/about" exact clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="link" />
+              </q-item-section>
+
+              <q-item-section>
+                About
+              </q-item-section>
+            </q-item>
+
+           
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top image" style="height: 185px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="../assets/purple-yeti-cartoon-sitting-on-chair.jpg">
+            </q-avatar>
+            <div class="text-weight-bold">Yeti</div>
+            <div>@yeti</div>
+          </div>
+        </q-img>
+      </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
@@ -48,6 +84,7 @@
 <script>
 import { defineComponent, ref } from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
+import { date } from 'quasar'
 
 const linksList = [
   {
@@ -111,6 +148,23 @@ export default defineComponent({
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
+  },
+  computed: {
+    todaysDate() {
+      const timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM')
+    }
   }
 })
 </script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  filter: grey;
+}
+.image {
+  background-color: rgb(216, 74, 216);
+}
+</style>
---
+++ quasar-todolist/src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5>About us</h5>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum nam, quaerat dicta alias modi voluptatum rerum 
+      deserunt ipsum doloribus praesentium, accusamus voluptatibus 
+      ex ab tempora provident. Cum tempora rerum assumenda!</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error debitis velit modi
+         ipsum iste facilis recusandae impedit hic, 
+        fuga vero ratione magnam porro explicabo illum amet reiciendis, deleniti aspernatur quaerat?</p>
+  </q-page>
+</template>
+
+<script>
+
+</script>
