b9b4f9dfe985b8f412fad0032318e0a171ac84b6
Veronika <nikazemlakova38@gmail.com>
2023-06-05 02:16:54 +0300

    add tasks

--- quasar-todolist/src/pages/Todo.vue
+++ quasar-todolist/src/pages/Todo.vue
@@ -1,9 +1,16 @@
 <template>
   <q-page class="bg-grey-3 column q-pa-lg">
+    <div class="row q-pa-sm bg-positive">
+      <q-input @keyup.enter="addTask" bg-color="white" class="col" square filled v-model="newTask" label="Todo" placeholder="Add some task" dense >
+        <template v-slot:append> 
+          <q-btn @click="addTask" color="primary" round dense flat icon="send" />
+        </template>
+      </q-input>
+    </div>
     <q-list separator bordered>
-      <q-item @click="task.done = !task.done" clickable :class="{ 'done bg-yellow-3': task.done }" v-for="(task, index) in tasks" :key="task.title" tag="label" v-ripple>
+      <q-item @click="task.done = !task.done" :class="{ 'done bg-green-2': task.done }" v-for="(task, index) in tasks" :key="task.title" tag="label" v-ripple>
         <q-item-section avatar>
-          <q-checkbox class="no-pointer-events" v-model="task.done" val="teal" color="$primary" />
+          <q-checkbox v-model="task.done" val="teal" color="primary" />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
@@ -20,6 +27,7 @@
 export default {
   data() {
     return {
+      newTask: '',
       tasks: [
         {
           id: 1,
@@ -41,7 +49,29 @@ export default {
   },
   methods: {
     deleteTask(index) {
-      this.tasks.splice(index, 1)
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'Do you really want to delete?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.tasks.splice(index, 1)
+        this.$q.notify({
+          message: 'Yeti deleted your todo.',
+          color:'accent',
+          avatar: 'https://img.freepik.com/free-photo/purple-yeti-cartoon-on-bicycle_23-2150248730.jpg?w=996&t=st=1685908119~exp=1685908719~hmac=11c91ff45b4a8ce515d67169390831ba79ab2d04af5aead9af9d3eb0c47c7275'
+        })
+      })   
+    },
+    addTask() {
+      this.tasks.push(
+        {
+          id: 5,
+          title: this.newTask, 
+          done: false
+        }
+      )
+      this.newTask = ''
     }
   }
 }
